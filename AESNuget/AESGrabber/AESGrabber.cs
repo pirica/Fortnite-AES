2c4bbc2f99e67d06a0608cf294f3d6ad7a9edbac
TheSingleOne <81616417+TheSingleOneYT@users.noreply.github.com>
2021-07-20 08:53:49 +0100

    Add 1.2.3 Files

---
+++ AESNuget/AESGrabber/AESGrabber.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Net;
+
+namespace FortniteAES.Grabber
+{
+    public class AESGrabber
+    {
+        const string aesurl = @"https://benbot.app/api/v1/aes";
+
+        public static string GetKeys()
+        {
+            var wc = new WebClient();
+            var keys = wc.DownloadString(aesurl).Split(new[] { '\r', '\n' })[0].Replace(",", "\n");
+            return keys;
+        }
+
+        public static string WriteKeysToFile(string path)
+        {
+            var wc = new WebClient();
+            var keys = wc.DownloadString(aesurl).Split(new[] { '\r', '\n' })[0].Replace(",", "\n");
+            File.WriteAllText(path, keys);
+            return path;
+        }
+
+        public static string GetMainKey()
+        {
+            var wc = new WebClient();
+            var keys = wc.DownloadString(aesurl).Split(new[] { '\r', '\n' })[0].Replace(",", "\n");
+
+            var keywithextra = keys.Remove(0, 69);
+            int noofchar = 0;
+
+            foreach (char c in keywithextra)
+            {
+                int temp = noofchar;
+                noofchar = temp + 1;
+            }
+
+            var key = "";
+
+            var tempnoc = noofchar;
+            noofchar = tempnoc - 66;
+
+            if (keywithextra.StartsWith("0x"))
+            {
+                key = keywithextra.Remove(66, noofchar);
+            }
+            else if (keywithextra.StartsWith("x"))
+            {
+                var temp = keywithextra;
+                key = keywithextra = "0" + temp;
+                key = keywithextra.Remove(66, noofchar);
+            }
+            else
+            {
+                throw new System.Exception("Failed. Key does not start with 0x.\n" + key);
+            }
+
+            return key;
+        }
+    }
+}
---
+++ AESNuget/ReadMe.txt
@@ -0,0 +1,20 @@
+﻿
+=====Fortnite AES Grabber: NuGet=====
+
+Grabs the latest AES keys for Fortnite (The Video Game).
+
+AESGrabber.GetKeys()
+-Grabs the latest AES keys.
+
+AESGrabber.WriteKeysToFile(string path)
+-Grabs the latest AES keys and writes them to a path of your choice
+
+AESGrabber.GetMainKey()
+-Attempts to get the latest main AES key.
+
+Fortnite is a trademark and is a copyrighted work of Epic Games, Inc. All rights reserved by Epic Games, Inc. This material is not official and is not
+endorsed by Epic Games, Inc.
+
+This tool uses https://benbot.app/api/v1/aes
+
+Thanks!
